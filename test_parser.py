fdd813fd7c6e4baf54a2f673ee48eb29bfb4afc4
Roman Fedyanin <roman@kula-tech.com>
2015-01-08 00:52:35 +0300

    FD-3 Добавлено приведение слов в нормальную форму

--- test_parser.py
+++ test_parser.py
@@ -2,7 +2,9 @@ from __future__ import division
 import os
 import nltk
 from nltk import FreqDist
+from nltk.stem.porter import *
 
+stemmer = PorterStemmer()
 number_of_documents = 0
 number_of_pos_documents = 0
 number_of_neg_documents = 0
@@ -10,6 +12,8 @@ docs = {}
 words_docs_count_dict = {}
 words_docs_pos_count_dict = {}
 words_docs_neg_count_dict = {}
+
+LEMMATIZE = True
 
 
 def load_data():
@@ -34,6 +38,7 @@ def change_docs_dict():
         # if docs[key] == 1:
         #     continue
         words = nltk.tokenize.word_tokenize(key)
+        lemmatize_words(words)
         freq_dict = FreqDist(words)
         normalize_freq_dist(freq_dict, words)
         dict_of_words_in_text = []
@@ -54,6 +59,7 @@ def add_text_to_corpus(text, negpos):
     text = text.lower()
     docs[text] = negpos
     words = nltk.tokenize.word_tokenize(text)
+    lemmatize_words(words)
     freq_dist = FreqDist(words)
     for k in freq_dist:
         words_docs_count_dict[k] = words_docs_count_dict.get(k, 0.0) + 1.0
@@ -75,6 +81,7 @@ def normalize_freq_dist(freq_dist, words):
 
 def make_text_with_tfidf(text):
     words = nltk.tokenize.word_tokenize(text)
+    lemmatize_words(words)
     freq_dict = FreqDist(words)
     normalize_freq_dist(freq_dict, words)
 
@@ -84,6 +91,13 @@ def make_text_with_tfidf(text):
         tfidf_all = number_of_documents * freq_dict[word] / words_docs_count_dict[word]
         text = text.replace(word + " ", word + "(%f) " % (tfidf_all, ))
     return text
+
+
+def lemmatize_words(words):
+    if not LEMMATIZE:
+        return
+    for i in range(len(words)):
+        words[i] = stemmer.stem(words[i])
 
 load_data()
 # text = "one thing to bear in mind is that there are many ways to text "
